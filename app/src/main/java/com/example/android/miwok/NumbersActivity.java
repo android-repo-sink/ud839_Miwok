0675712acedb87c6e8069448e24a4503e12390a4
sanjarcode <sanjarcode@gmail.com>
2020-12-20 04:48:30 +0530

    List populated

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,8 +12,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        populateList();
+    }
 
-        // TODO: Add number<String> array
+    private void populateList() {
         String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        LinearLayout numbers_list = (LinearLayout) findViewById(R.id.numbers_list);
+        for (String number : numbers) {
+            TextView number_item = new TextView(this);
+            number_item.setTextSize(24);
+            number_item.setText((number.charAt(0) + "").toUpperCase() + number.substring(1));
+            if (numbers_list != null)
+                numbers_list.addView(number_item);
+        }
     }
 }
