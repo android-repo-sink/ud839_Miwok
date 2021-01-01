3003c220d168107e59db171cb9216793ba242a4c
sanjarcode <sanjarcode@gmail.com>
2021-01-01 23:56:46 +0530

    ListView added for all 4 sections

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,45 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+        populateList();
     }
+
+
+    // populate using ListView and ArrayAdapter
+    private void populateList() {
+
+        // DS - data store
+        // list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        // ArrayAdapter - concrete class, displays using getView. Have to specify type of content
+        // constructor - context, listitem_view, content array
+        WordAdapter arrayAdapter = new WordAdapter(this, R.layout.list_item, words);
+
+        // ListView takes the array as adapter
+        ListView lv = (ListView) findViewById(R.id.word_list);
+
+        // attach adapter to ListView
+        lv.setAdapter(arrayAdapter);
+    }
+
+
 }
