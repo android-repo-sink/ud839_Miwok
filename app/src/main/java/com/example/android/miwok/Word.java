0d6d0f211ec88740fd6245bd889ed0eed3fa994e
sanjarcode <sanjarcode@gmail.com>
2021-01-03 07:32:03 +0530

    Word class changed

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 // stores two strings, viz English word and its Miwok translation. This is the DS item type.
 // It is required by ArrayAdapter
 
+import android.content.Intent;
+
 public class Word {
     //    Default translation for the word
     private String mDefaultTranslation;
@@ -10,18 +12,31 @@ public class Word {
     //  Miwok translation for the word
     private String mMiwokTranslation;
 
+    // Image resource id
+    private Integer mImageResourceId;
+
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    //    Get the default translation of the word.
+    public Word(String mDefaultTranslation, String mMiwokTranslation, Integer mImageResourceId) {
+        this.mImageResourceId = mImageResourceId;
+    }
+
+    // Get the default translation of the word.
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-    //    Get the Miwok translation of the word.
+    // Get the Miwok translation of the word.
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+
+    // Get the image resource id
+    public Integer getmImageResourceId() {
+        return mImageResourceId;
+    }
+
 }
